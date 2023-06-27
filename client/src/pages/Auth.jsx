3d849902ac43cefc426211513f1dcb15a831603e
ruhidniftiyev <tigerrn22@gmail.com>
2023-06-27 23:50:33 +0400

    add-auth

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Auth = () => {
   return (
-    <div className=''>
-
+    <div className="bg-slate-300 w-screen h-screen flex flex-col align-middle justify-center">
+      <div className="flex justify-center">
+        <p>Registration Form</p>
+      </div>
+      <div className="flex justify-center">
+        <form className="flex flex-col justify-center">
+          <label>E-mail</label>
+          <input type="text" placeholder="E-mail" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
