7814e3054f86a2de0696d62921570aaa14a7ed79
ruhidniftiyev <tigerrn22@gmail.com>
2023-06-27 21:22:40 +0400

    add redirect

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
+import Shop from '../pages/Shop';
 
 const AppRouter = () => {
   const isAuth = false;
@@ -13,6 +14,7 @@ const AppRouter = () => {
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} Component={Component} />
       ))}
+      <Route path="*" Component={Shop} />
     </Routes>
   );
 };
